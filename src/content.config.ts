aabaad2134be6761407204b99b66c108c265548b
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2026-07-05 14:01:59 -0500

    create relation between blog and author collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { defineCollection } from "astro/content/config";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
+import { reference } from "astro:content";
 
 const blogCollection = defineCollection({
     loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/blog" }),
@@ -9,11 +10,20 @@ const blogCollection = defineCollection({
         date: z.date(),
         description: z.string(),
         image: image(),
-        author: z.string(),
+        author: reference("author"),
         tags: z.array(z.string())
     })
-})
+});
+
+const authorCollection = defineCollection({
+    loader: glob({ pattern: "**/[^_]*.{yml}", base: "./src/content/author" }),
+    schema: ({ image }) => z.object({
+        name: z.string(),
+        avatar: image(),
+    })
+});
 
 export const collections = {
-    "blog": blogCollection
+    "blog": blogCollection,
+    "author": authorCollection
 }
